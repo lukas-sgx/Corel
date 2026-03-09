5ff22fadba3a698a4c8769c9d2c1803b24452e4d
lukas-sgx <lukas.soigneux@epitech.eu>
2026-03-10 00:41:24 +0100

    [+] Add basic helper command

--- cmd/cli.go
+++ cmd/cli.go
@@ -1,15 +1,46 @@
 package cmd
 
 import (
+	"github.com/lukas-sgx/corel/cmd/client"
+	"github.com/lukas-sgx/corel/cmd/server"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "corel",
-	Short: "Corel is a tool to manage your projects",
-	Long:  `Corel is a tool to manage your projects, it allows you to create, delete, list and update your projects.`,
+	Short: "Corel CLI tool",
+	Long:  `Lightweight, cross-platform network relay and lateral movement agent for containerized environments.`,
+	CompletionOptions: cobra.CompletionOptions{
+		DisableDefaultCmd: true,
+	},
+}
+
+func init_templates() {
+	rootCmd.SetHelpCommand(&cobra.Command{Use: "no-help", Hidden: true})
+	rootCmd.SetUsageTemplate(`Usage:
+  {{.UseLine}}{{if .HasAvailableSubCommands}}
+  {{.CommandPath}} [module]{{end}}
+
+Available Modules:{{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}{{if .IsAvailableCommand}}
+  {{rpad .Name .NamePadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+Flags:
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}
+
+Use "{{.CommandPath}} [command] --help" for more information about a command.
+`)
+
+	rootCmd.Version = "0.1.0"
+	rootCmd.SetVersionTemplate("Corel version {{.Version}}\n")
 }
 
 func Cli() error {
+	init_templates()
+	rootCmd.AddGroup(&cobra.Group{
+		ID:    "modules",
+		Title: "Available Modules:",
+	})
+	rootCmd.AddCommand(client.ClientCmd)
+	rootCmd.AddCommand(server.ServerCmd)
 	return rootCmd.Execute()
 }
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -3,3 +3,17 @@ package server
 import (
 	"github.com/spf13/cobra"
 )
+
+var ServerCmd = &cobra.Command{
+	Use:     "server",
+	Short:   "Start the Corel server",
+	Long:    `Start the Corel server, it will listen on the specified port and handle incoming requests.`,
+	GroupID: "modules",
+	Run: func(cmd *cobra.Command, args []string) {
+		println("Server started")
+	},
+}
+
+func Server() error {
+	return ServerCmd.Execute()
+}
