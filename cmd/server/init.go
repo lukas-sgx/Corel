5db0cfdc407cd4e3f71964b7a72f12e586977624
lukas-sgx <lukas.soigneux@epitech.eu>
2026-03-12 00:00:45 +0100

    feat: add connection handler

--- cmd/server/init.go
+++ cmd/server/init.go
@@ -12,6 +12,27 @@ ClearScreen + Blue +
 "[ STATUS ::        LISTENING ]\n" +
 "==============================\n" + Reset
 
-func InitHeader() {
+func initHeader() {
 	fmt.Println(header)
+	fmt.Println(setBlue("[SCAN]") + " Listening for incoming beacons...")
+}
+
+func isMeshSync(mesh bool) {
+	if (mesh == true) {
+		fmt.Println(setBlue("[SCAN]") + " Global mesh synchronization: ACTIVE signification\n");
+	} else {
+		fmt.Println()
+	}
+}
+
+func listenerInfo(listenAddr string, port int) {
+	fmt.Println(setRed("[SERVER]") + " Server active on:")
+	if (listenAddr == "127.0.0.1" || listenAddr == "0.0.0.0") {
+		fmt.Printf(setRed("[SERVER]") + "   - Internal: tcp://127.0.0.1:%d\n", port)
+	}
+	if (listenAddr != "127.0.0.1") {
+		fmt.Printf(setRed("[SERVER]") + "   - External: tcp://%s:%d\n", listenAddr, port)
+	}
+	fmt.Println()
+	fmt.Println(setGreen("[READY]") + "  Waiting for Client Handshakes...")
 }
